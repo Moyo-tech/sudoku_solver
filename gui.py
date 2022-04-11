7bdcf3ece56cc92164de6480411243f56795b1be
Moyo-tech <m.weke@alustudent.com>
2022-04-11 13:27:43 +0200

    First commit

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 from tkinter import *
-from main import solver
+from solver import solver
 
 root = Tk()
-root.title("Sudoku Solver")  # title
+root.title("Sudoku Solver") # title
 root.option_add('*Font', 'Montserrat ')  # to change text font
 root.geometry("455x450")  # the dimensions of the GUI
 root.configure(bg='#D0D0FF')
@@ -10,39 +10,33 @@ root.configure(bg='#D0D0FF')
 label = Label(root, text="Fill in the numbers and click solve").grid(row=0, column=1, columnspan=10)
 
 errLabel = Label(root, text="", fg="red")
-errLabel.grid(row=15, column =1 ,columnspan=10, pady=5)
+errLabel.grid(row=15, column=1, columnspan=10, pady=5)
 
 solvedLabel = Label(root, text="", fg="green")
 solvedLabel.grid(row=15, column=1, columnspan=10, pady=5)
 
 cells = {}
 
-
 def ValidateNumber(P):
     out = (P.isdigit() or P == "") and len(P) < 2
     return out
 
-
 reg = root.register(ValidateNumber)
-
 
 def draw3x3Grid(row, column, bgcolor):
     for i in range(3):
         for j in range(3):
-            e = Entry(root, width=5, bg=bgcolor, justify="center",
-                      validate="key", validatecommand=(reg, "%P"))
-            e.grid(row=row + i + 1, column=column + j + 1,
-                   sticky="nsew", padx=1, pady=1, ipady=5)
-            cells[(row + i + i, column + j + 1)] = e
-
+            e = Entry(root, width=5, bg=bgcolor, justify="center", validate="key", validatecommand=(reg, "%P"))
+            e.grid(row=row+i+1, column=column+j+1, sticky="nsew", padx=1, pady=1, ipady=5)
+            cells[(row+i+1, column+j+1)] = e
 
 def draw9x9Grid():
     color = "#ffffff"
     for rowNo in range(1, 10, 3):
-        for colNo in range(0, 9, 3):
+        for colNo in range(0,9,3):
             draw3x3Grid(rowNo, colNo, color)
             if color == "#ffffff":
-                color = "#ffffD0"
+                color = "#ffffd0"
             else:
                 color = "#ffffff"
 
@@ -51,7 +45,7 @@ def clearValues():
     errLabel.configure(text="")
     solvedLabel.configure(text="")
     for row in range(2, 11):
-        for col in range(1, 10):
+        for col in range(1 , 10):
             cell = cells[(row, col)]
             cell.delete(0, "end")
 
@@ -72,13 +66,11 @@ def getValues():
         board.append(rows)
     updateValues(board)
 
-
 btn = Button(root, command=getValues, text="Solve", width=10)
 btn.grid(row=20, column=1, columnspan=5, pady=20)
 
 btn = Button(root, command=clearValues, text="Clear", width=10)
 btn.grid(row=20, column=5, columnspan=5, pady=20)
-
 
 #Updates the cells and displays the solution of sudoku 
 def updateValues(s):
@@ -86,13 +78,20 @@ def updateValues(s):
     if sol != "no":
         for rows in range(2, 11):
             for col in range(1, 10):
-                cells[(rows, col)].delete(0, "end")  
-                cells[(rows, col)].insert(0, sol[rows-2][col-1]) #Inserting the value 
-        solvedLabel.configure(text="Sudoku solved")
-
+                cells[(rows, col)].delete(0, "end")
+                cells[(rows, col)].insert(0, sol[rows -2 ][col - 1])
+        solvedLabel.configure(text="Sudoku solved!")
     else:
         errLabel.configure(text="No solution exists for this sudoku")
 
-
 draw9x9Grid()
 root.mainloop()
+
+    
+
+
+
+            
+
+
+    
